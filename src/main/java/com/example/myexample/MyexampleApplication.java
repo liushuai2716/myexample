43c739372fb6dac5c56faf4f318d3b2406e8af9f
liushuai <liushuai2716@163.com>
2020-09-13 18:04:16 +0800

    初始化项目

---
+++ src/main/java/com/example/myexample/MyexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.myexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myexample/MyexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
